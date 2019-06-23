ca065b3fd804eb0d3fd1bfe009bc151ed410b242
sonamk15 <sonamk18@navgurukul.org>
2019-06-23 10:06:01 +0530

    initial commit

---
+++ SockMerchant.py
@@ -0,0 +1,12 @@
+def sockMerchant(ar):
+    ar.sort()
+    # print ar
+    new=[]
+    i=0
+    while i<len(ar)-1:
+        if ar[i] == ar[i+1]:
+            new.insert(0,[ar[i],ar[i+1]])
+            ar.remove(ar[i])
+        i+=1
+    return len(new)
+print sockMerchant([10, 20, 20, 10, 10, 30, 50, 10, 20])
